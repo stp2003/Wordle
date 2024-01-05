4d0f81babfe9fd343f748f6746d436c9fe2756b4
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-01-05 17:29:44 +0530

    basic keyboard ui made

--- lib/components/keyboard_row.dart
+++ lib/components/keyboard_row.dart
@@ -14,13 +14,36 @@ class KeyboardRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     int index = 0;
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: keysMap.entries.map(
         (e) {
           index++;
           if (index >= min && index <= max) {
-            return Text(e.key);
+            return Padding(
+              padding: EdgeInsets.all(size.width * 0.006),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(6.0),
+                child: Container(
+                  color: Colors.orangeAccent,
+                  width: e.key == 'ENTER' || e.key == 'BACK'
+                      ? size.width * 0.13
+                      : size.width * 0.085,
+                  height: size.height * 0.085,
+                  child: Material(
+                    color: Colors.white24,
+                    child: InkWell(
+                      onTap: () {},
+                      child: Center(
+                        child: Text(e.key),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
           } else {
             return const SizedBox();
           }
