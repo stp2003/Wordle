ffe4d3c6c1a198a5d89ce6a44334475c1f55ff65
stp2003 <100586951+stp2003@users.noreply.github.com>
2024-01-13 14:04:43 +0530

    bugs fixed

--- lib/components/keyboard_row.dart
+++ lib/components/keyboard_row.dart
@@ -23,63 +23,66 @@ class KeyboardRow extends StatelessWidget {
     return Consumer<Controller>(
       builder: (_, notifier, __) {
         int index = 0;
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: keysMap.entries.map(
-            (e) {
-              index++;
-              if (index >= min && index <= max) {
-                Color color = Theme.of(context).primaryColorLight;
-                Color keyColor = Colors.white;
-                if (e.value == AnswerStage.correct) {
-                  color = correctGreen;
-                } else if (e.value == AnswerStage.contains) {
-                  color = containsYellow;
-                } else if (e.value == AnswerStage.incorrect) {
-                  color = Theme.of(context).primaryColorDark;
-                } else {
-                  keyColor = Theme.of(context).textTheme.bodyMedium?.color ??
-                      Colors.black;
-                }
-                return Padding(
-                  padding: EdgeInsets.all(size.width * 0.006),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(6.0),
-                    child: SizedBox(
-                      width: e.key == 'ENTER' || e.key == 'BACK'
-                          ? size.width * 0.13
-                          : size.width * 0.085,
-                      height: size.height * 0.085,
-                      child: Material(
-                        color: color,
-                        child: InkWell(
-                          onTap: () {
-                            Provider.of<Controller>(context, listen: false)
-                                .setKeyTapped(value: e.key);
-                          },
-                          child: Center(
-                            child: e.key == 'BACK'
-                                ? const Icon(Icons.backspace_outlined)
-                                : Text(
-                                    e.key,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium
-                                        ?.copyWith(
-                                          color: keyColor,
-                                        ),
-                                  ),
+        return IgnorePointer(
+          ignoring: notifier.gameCompleted,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: keysMap.entries.map(
+              (e) {
+                index++;
+                if (index >= min && index <= max) {
+                  Color color = Theme.of(context).primaryColorLight;
+                  Color keyColor = Colors.white;
+                  if (e.value == AnswerStage.correct) {
+                    color = correctGreen;
+                  } else if (e.value == AnswerStage.contains) {
+                    color = containsYellow;
+                  } else if (e.value == AnswerStage.incorrect) {
+                    color = Theme.of(context).primaryColorDark;
+                  } else {
+                    keyColor = Theme.of(context).textTheme.bodyMedium?.color ??
+                        Colors.black;
+                  }
+                  return Padding(
+                    padding: EdgeInsets.all(size.width * 0.006),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(6.0),
+                      child: SizedBox(
+                        width: e.key == 'ENTER' || e.key == 'BACK'
+                            ? size.width * 0.13
+                            : size.width * 0.085,
+                        height: size.height * 0.085,
+                        child: Material(
+                          color: color,
+                          child: InkWell(
+                            onTap: () {
+                              Provider.of<Controller>(context, listen: false)
+                                  .setKeyTapped(value: e.key);
+                            },
+                            child: Center(
+                              child: e.key == 'BACK'
+                                  ? const Icon(Icons.backspace_outlined)
+                                  : Text(
+                                      e.key,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium
+                                          ?.copyWith(
+                                            color: keyColor,
+                                          ),
+                                    ),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              } else {
-                return const SizedBox();
-              }
-            },
-          ).toList(),
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ).toList(),
+          ),
         );
       },
     );
